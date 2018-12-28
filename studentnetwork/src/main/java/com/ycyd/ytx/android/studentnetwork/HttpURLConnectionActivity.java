4802ae53ae672ef0abe7e259115e42c958b73d37
ytx012 <13607201698@139.com>
2018-12-28 19:43:31 +0800

    第二次提交

--- studentnetwork/src/main/java/com/ycyd/ytx/android/studentnetwork/HttpURLConnectionActivity.java
+++ studentnetwork/src/main/java/com/ycyd/ytx/android/studentnetwork/HttpURLConnectionActivity.java
@@ -7,10 +7,15 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 public class HttpURLConnectionActivity extends AppCompatActivity {
@@ -19,12 +24,16 @@ public class HttpURLConnectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_http_urlconnection);
-        Button sendRequest=(Button) findViewById(R.id.id_send);
+        final Button sendRequest=(Button) findViewById(R.id.id_send);
         responseTextView=(TextView) findViewById(R.id.id_response);
         sendRequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 responseTextView.setText("");
+                sendRequestWithHttpURLConnec();
+            }
+
+            private void sendRequestWithHttpURLConnec() {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
@@ -47,28 +56,26 @@ public class HttpURLConnectionActivity extends AppCompatActivity {
                                 response.append(line);
                             }
  */
+
+/*GET模式
                             // 1. 得到访问地址的URL
-                            URL url = new URL("https://www.baidu.com");
-                            //URL url = new URL("http://111.47.64.131:8091/app/QueryVersionJSON.jsp?name=Android.OutboundCall");
-                            //URL url = new URL("http://111.47.64.131:8091/app/QueryVersionJSON.jsp");
+                            //URL url = new URL("https://www.baidu.com");
+                            URL url = new URL("http://111.47.64.131:8091/app/QueryVersionJSON.jsp?name=Android.OutboundCall");
                             // 2. 得到网络访问对象java.net.HttpURLConnection
                             connection = (HttpURLConnection) url.openConnection();
-                            /* 3. 设置请求参数（过期时间，输入、输出流、访问方式），以流的形式进行连接 */
+                            // 3. 设置请求参数（过期时间，输入、输出流、访问方式），以流的形式进行连接
+                            // 设置请求方式
+                            connection.setRequestMethod("GET");
                             // 设置是否向HttpURLConnection输出
                             connection.setDoOutput(false);
                             // 设置是否从httpUrlConnection读入
                             connection.setDoInput(true);
-                            // 设置请求方式
-                            connection.setRequestMethod("GET");
                             // 设置是否使用缓存
                             connection.setUseCaches(true);
                             // 设置此 HttpURLConnection 实例是否应该自动执行 HTTP 重定向
                             connection.setInstanceFollowRedirects(true);
                             // 设置超时时间
                             connection.setConnectTimeout(3000);
-
-
-
                             //name=Android.OutboundCall
                             // 连接
                             connection.connect();
@@ -86,10 +93,73 @@ public class HttpURLConnectionActivity extends AppCompatActivity {
                             }
                             // 6. 断开连接，释放资源
                             connection.disconnect();
+GET模式*/
+
+
+
+//*Post模式
+                            // 1. 得到访问地址的URL
+                            //URL url = new URL("http://111.47.64.131:8091/app/QueryVersionJSON.jsp?name=Android.OutboundCall");
+                            //URL url = new URL("http://10.28.4.176:8091/app/test.xml");
+                            //http://www.w3school.com.cn/example/xmle/cd_catalog.xml
+                            URL url = new URL("http://10.28.4.176:8091/app/test.txt");
+                            // 2. 得到网络访问对象java.net.HttpURLConnection
+                            connection = (HttpURLConnection) url.openConnection();
+                            // 3. 设置请求参数（过期时间，输入、输出流、访问方式），以流的形式进行连接
+                            // 设置请求方式
+                            //connection.setRequestMethod("POST");
+                            //connection.setRequestMethod("GET");
+                            // 设置是否向HttpURLConnection输出
+                            //connection.setDoOutput(true);
+                            // 设置是否从httpUrlConnection读入
+                            //connection.setDoInput(true);
+                            // 设置是否使用缓存
+                            //connection.setUseCaches(true);
+                            // 设置此 HttpURLConnection 实例是否应该自动执行 HTTP 重定向
+                            //connection.setInstanceFollowRedirects(true);
+                            // 设置超时时间
+                            //connection.setConnectTimeout(3000);
+                            //connection.setRequestProperty("Accept", "text/xml");
+                            //DataOutputStream out=new DataOutputStream(connection.getOutputStream());
+                            //out.writeBytes("name=Android.OutboundCall");//&paramter=XXXXX
+
+                            // 连接
+                            //connection.connect();
+                            // 4. 得到响应状态码的返回值 responseCode
+                            int code = connection.getResponseCode();
+                            // 5. 如果返回值正常，数据在网络中是以流的形式得到服务端返回的数据
+                            StringBuilder msg = new StringBuilder();
+                            if (code == 200) { // 正常响应
+                                // 从流中读取响应信息
+                                reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                                String line = null;
+                                while ((line = reader.readLine()) != null) { // 循环从流中读取
+                                    msg.append(line);
+                                }
+                            }
+                            // 6. 断开连接，释放资源
+                            connection.disconnect();
+// Post模式*/
+
+
                             // 显示响应结果
                             showResponse(msg.toString());
-                        }catch (Exception ex){
-                            ex.printStackTrace();
+                        } catch (MalformedURLException e) {
+                            // URL格式错误
+                            System.out.println("ytx1");
+                            e.printStackTrace();
+                        } catch (UnsupportedEncodingException e) {
+                            // 不支持你设置的编码
+                            System.out.println("ytx2");
+                            e.printStackTrace();
+                        } catch (ProtocolException e) {
+                            // 请求方式不支持
+                            System.out.println("ytx3");
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            // 输入输出通讯出错
+                            System.out.println("ytx4");
+                            e.printStackTrace();
                         }finally {
                             // 关闭流
                             if(reader!=null){
--- studentnetwork/src/main/java/com/ycyd/ytx/android/studentnetwork/OkHttpActivity.java
+++ studentnetwork/src/main/java/com/ycyd/ytx/android/studentnetwork/OkHttpActivity.java
@@ -1,23 +1,43 @@
 package com.ycyd.ytx.android.studentnetwork;
 
+import android.media.session.MediaSession;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.xml.sax.Attributes;
+
+import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.spec.ECField;
+import java.util.List;
 
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class OkHttpActivity extends AppCompatActivity {
@@ -26,13 +46,24 @@ public class OkHttpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_http_urlconnection);
-        Button sendRequest=(Button) findViewById(R.id.id_send);
+        final Button sendRequest=(Button) findViewById(R.id.id_send);
         responseTextView=(TextView) findViewById(R.id.id_response);
+
+
+
+
+
         sendRequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 responseTextView.setText("");
+                sendRequestWithOkHttp();
+
+            }
+
+            private void sendRequestWithOkHttp() {
                 new Thread(new Runnable() {
+                    //ver对象
                     class Ver {
                         protected int versionCode;
                         public int getVersionCode(){
@@ -47,31 +78,113 @@ public class OkHttpActivity extends AppCompatActivity {
                     public void run() {
                         try{
                             OkHttpClient client=new OkHttpClient();
+                            //*GET
                             Request request=new Request.Builder()
                                     //.url("http://www.baidu.com")
-                                    .url("http://111.47.64.131:8091/app/QueryVersionJSON.jsp?name=Android.outboundcall")
+                                    //.url("http://111.47.64.131:8091/app/QueryVersionJSON.jsp?name=Android.outboundcall")
+                                    .url("http://10.28.4.176:8091/app/test.xml")
+                                    //.url("http://www.w3school.com.cn/example/xmle/cd_catalog.xml")
                                     .build();
+                            /*/
+                            /*POST
+                            RequestBody requestBody=new FormBody.Builder()
+                                    .add("name","Android.outboundcall")
+                                    //.add(name,value)
+                                    .build();
+                            Request request=new Request.Builder()
+                                    .url("http://111.47.64.131:8091/app/QueryVersionJSON.jsp")
+                                    .post(requestBody)
+                                    .build();
+                            */
                             Response response=client.newCall(request).execute();
                             String reader=response.body().string();
                             //showResponse(reader);
-                            parseJSONWithJSONObject(reader);
+                            //解析JSON
+                            //parseJSON(reader);
+                            parseXML(reader);
+
                         }catch (Exception e){
                             e.printStackTrace();
                         }
                     }
 
-                    private void parseJSONWithJSONObject(String reader) {
+                    private void parseXML(final String s) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                //parseXMLPull(s);
+                                parseXMLSAX(s);
+                            }
+
+                        });
+                    }
+
+
+                    private void parseXMLPull(String reader) {
+                        StringBuilder stringResult=new StringBuilder();
+                        try {
+                            //*Pull解析
+                            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                            // 由工厂创建一个解析器对象
+                            XmlPullParser parser = factory.newPullParser();
+                            // 打开xml文档对应的输入流,填写xml文档的路径
+                            //parser.setInput(new FileInputStream("src/xmlwendang/student.xml"), "UTF-8");
+                            parser.setInput(new StringReader(reader));
+                            // 获得事件类型
+                            int eventType = parser.getEventType();
+                            //System.out.println(eventType == XmlPullParser.START_DOCUMENT);
+                            String sName="",iPrince="";
+                            while (eventType != XmlPullParser.END_DOCUMENT) {
+                                switch (eventType) {
+                                    case XmlPullParser.START_DOCUMENT:
+                                        System.out.println("读取开始,开始文档!");
+                                        break;
+                                    case XmlPullParser.START_TAG:
+                                        String tagName = parser.getName();
+                                        if (tagName.equals("name")) {
+                                            sName = parser.nextText();
+                                        }
+                                        if (tagName.equals("price")) {
+                                            iPrince = parser.nextText();
+                                        }
+                                        break;
+                                    case XmlPullParser.END_TAG:
+                                        if (parser.getName().equals("book")) {
+                                            //System.out.println("读取进度-结束标签:" + parser.getName());
+                                            stringResult.append("书名：" + sName + ";价格:" + iPrince);
+                                        } else if (parser.getName().equals("books")) {
+                                            //System.out.println("读取完成-结束根目录:" + parser.getName());
+                                        }
+                                        break;
+                                }
+                                // 读取下一个节点
+                                eventType = parser.next();
+                            }
+                            //System.out.println("读取完成，文档结束!");
+                            //showResponse(stringResult.toString());
+                            responseTextView.setText(stringResult.toString());
+
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+
+                    private void parseJSON(String reader) {
                         try{
+                            //JSONObject解析
                             //JSONObject jsonObject=new JSONObject(reader);
                             //final String ver=String.valueOf(jsonObject.getInt("versionCode"));
-                             Gson gson=new Gson();
-                            final Ver ver=gson.fromJson(reader,Ver.class);
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    responseTextView.setText(String.valueOf(ver.getVersionCode()));
-                                }
-                            });
+                            //若是对象列表等使用
+                            //JSONArray jsonArray=new JSONArray(reader);
+                            //JSONObject jsonObject=jsonArray.getJSONObject(i);
+                            //showResponse(ver);
+
+                            //google GSON解析
+                            Gson gson=new Gson();
+                            Ver ver=gson.fromJson(reader,Ver.class);
+                            //若是对象列表等使用
+                            //List<Ver> list=gson.fromJson(reader，new TypeToken<List<Ver>>(){}.getType());
+                            showResponse(String.valueOf(ver.getVersionCode()));
                         }catch (Exception e){
                             e.printStackTrace();
                         }
@@ -88,5 +201,68 @@ public class OkHttpActivity extends AppCompatActivity {
                 }).start();
             }
         });
+    }
+
+    private void parseXMLSAX(String reader) {
+        //SAX解析
+        try {
+            //1.创建解析工厂
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            //2.得到解析器
+            //3得到解读器
+            XMLReader xmlReader = factory.newSAXParser().getXMLReader();
+            //设置内容处理器
+            xmlReader.setContentHandler(new ContentHandlerpro());
+            //读取xml的文档内容
+            Log.d("httpreader", "parseXMLSAX: "+reader);
+            String tmp=new String(reader);
+            xmlReader.parse(tmp);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    //SAX解析接口
+    class ContentHandlerpro extends DefaultHandler {
+        private String TAG="ContentHandlerpro";
+        private String nodeName;
+        private StringBuilder sName;
+        private StringBuilder iPrince;
+        private StringBuilder result;
+
+        @Override
+        public void endDocument() throws SAXException {
+            responseTextView.setText(result.toString());
+        }
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+            Log.d(TAG, "startElement: "+localName);
+
+            nodeName=localName;
+        }
+        @Override
+        public void startDocument() throws SAXException {
+            sName=new StringBuilder();
+            iPrince=new StringBuilder();
+            result=new StringBuilder();
+        }
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            if("book".equals(localName)){
+                result.append("书名:"+sName.toString().trim()+";");
+                result.append("价格:"+iPrince.toString().trim()+"\r\n");
+                sName.setLength(0);
+                iPrince.setLength(0);
+            }
+        }
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            if("name".equals(nodeName)){
+                sName.append(ch,start,length);
+            }else if("price".equals(nodeName)){
+                iPrince.append(ch,start,length);
+            }
+        }
+
     }
 }
