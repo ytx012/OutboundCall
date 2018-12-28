19ac6d74738cffa21dbf32541d9306adcf5eeae0
ytx012 <13607201698@139.com>
2018-12-29 01:31:32 +0800

    第三次提交

--- studentnetwork/src/main/java/com/ycyd/ytx/android/studentnetwork/OkHttpActivity.java
+++ studentnetwork/src/main/java/com/ycyd/ytx/android/studentnetwork/OkHttpActivity.java
@@ -40,49 +40,37 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
+import org.xml.sax.InputSource;
+
 public class OkHttpActivity extends AppCompatActivity {
     TextView responseTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_http_urlconnection);
-        final Button sendRequest=(Button) findViewById(R.id.id_send);
-        responseTextView=(TextView) findViewById(R.id.id_response);
-
-
-
-
-
+        final Button sendRequest = (Button) findViewById(R.id.id_send);
+        responseTextView = (TextView) findViewById(R.id.id_response);
         sendRequest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 responseTextView.setText("");
-                sendRequestWithOkHttp();
-
+                String sURL = "http://111.47.64.131:8091/app/test.xml";
+                //sendRequestWithOkHttp(sURL);
+                parseXMLSAX(sURL);
             }
 
-            private void sendRequestWithOkHttp() {
+            private void sendRequestWithOkHttp(final String sURL) {
                 new Thread(new Runnable() {
-                    //ver对象
-                    class Ver {
-                        protected int versionCode;
-                        public int getVersionCode(){
-                            return this.versionCode;
-                        }
-                        public void setVersionCode(int versionCode){
-                            this.versionCode=versionCode;
-                        }
-                    }
-
                     @Override
                     public void run() {
-                        try{
-                            OkHttpClient client=new OkHttpClient();
+                        try {
+                            OkHttpClient client = new OkHttpClient();
                             //*GET
-                            Request request=new Request.Builder()
+                            Request request = new Request.Builder()
                                     //.url("http://www.baidu.com")
                                     //.url("http://111.47.64.131:8091/app/QueryVersionJSON.jsp?name=Android.outboundcall")
-                                    .url("http://10.28.4.176:8091/app/test.xml")
+                                    .url(sURL)
                                     //.url("http://www.w3school.com.cn/example/xmle/cd_catalog.xml")
                                     .build();
                             /*/
@@ -96,32 +84,54 @@ public class OkHttpActivity extends AppCompatActivity {
                                     .post(requestBody)
                                     .build();
                             */
-                            Response response=client.newCall(request).execute();
-                            String reader=response.body().string();
+                            Response response = client.newCall(request).execute();
+                            String reader = response.body().string();
                             //showResponse(reader);
                             //解析JSON
                             //parseJSON(reader);
-                            parseXML(reader);
+                            parseXMLPull(reader);
 
-                        }catch (Exception e){
+                        } catch (Exception e) {
                             e.printStackTrace();
                         }
                     }
 
-                    private void parseXML(final String s) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                //parseXMLPull(s);
-                                parseXMLSAX(s);
-                            }
+                    private void parseJSON(String reader) {
+                        try {
+                            //JSONObject解析
+                            //JSONObject jsonObject=new JSONObject(reader);
+                            //final String ver=String.valueOf(jsonObject.getInt("versionCode"));
+                            //若是对象列表等使用
+                            //JSONArray jsonArray=new JSONArray(reader);
+                            //JSONObject jsonObject=jsonArray.getJSONObject(i);
+                            //showResponse(ver);
 
-                        });
+                            //google GSON解析
+                            Gson gson = new Gson();
+                            Ver ver = gson.fromJson(reader, Ver.class);
+                            //若是对象列表等使用
+                            //List<Ver> list=gson.fromJson(reader，new TypeToken<List<Ver>>(){}.getType());
+                            showResponse(String.valueOf(ver.getVersionCode()));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
 
+                    //ver对象
+                    class Ver {
+                        protected int versionCode;
+
+                        public int getVersionCode() {
+                            return this.versionCode;
+                        }
+
+                        public void setVersionCode(int versionCode) {
+                            this.versionCode = versionCode;
+                        }
+                    }
 
                     private void parseXMLPull(String reader) {
-                        StringBuilder stringResult=new StringBuilder();
+                        StringBuilder stringResult = new StringBuilder();
                         try {
                             //*Pull解析
                             XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
@@ -133,7 +143,7 @@ public class OkHttpActivity extends AppCompatActivity {
                             // 获得事件类型
                             int eventType = parser.getEventType();
                             //System.out.println(eventType == XmlPullParser.START_DOCUMENT);
-                            String sName="",iPrince="";
+                            String sName = "", iPrince = "";
                             while (eventType != XmlPullParser.END_DOCUMENT) {
                                 switch (eventType) {
                                     case XmlPullParser.START_DOCUMENT:
@@ -161,31 +171,8 @@ public class OkHttpActivity extends AppCompatActivity {
                                 eventType = parser.next();
                             }
                             //System.out.println("读取完成，文档结束!");
-                            //showResponse(stringResult.toString());
-                            responseTextView.setText(stringResult.toString());
-
-                        }catch (Exception e){
-                            e.printStackTrace();
-                        }
-                    }
-
-                    private void parseJSON(String reader) {
-                        try{
-                            //JSONObject解析
-                            //JSONObject jsonObject=new JSONObject(reader);
-                            //final String ver=String.valueOf(jsonObject.getInt("versionCode"));
-                            //若是对象列表等使用
-                            //JSONArray jsonArray=new JSONArray(reader);
-                            //JSONObject jsonObject=jsonArray.getJSONObject(i);
-                            //showResponse(ver);
-
-                            //google GSON解析
-                            Gson gson=new Gson();
-                            Ver ver=gson.fromJson(reader,Ver.class);
-                            //若是对象列表等使用
-                            //List<Ver> list=gson.fromJson(reader，new TypeToken<List<Ver>>(){}.getType());
-                            showResponse(String.valueOf(ver.getVersionCode()));
-                        }catch (Exception e){
+                            showResponse(stringResult.toString());
+                        } catch (Exception e) {
                             e.printStackTrace();
                         }
                     }
@@ -200,69 +187,95 @@ public class OkHttpActivity extends AppCompatActivity {
                     }
                 }).start();
             }
+
+            private void parseXMLSAX(final String sURL) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            //SAX解析
+                            //1.创建解析工厂
+                            SAXParserFactory factory = SAXParserFactory.newInstance();
+                            //2.得到解析器
+                            //3得到解读器
+                            XMLReader xmlReader = factory.newSAXParser().getXMLReader();
+                            //设置内容处理器
+                            xmlReader.setContentHandler(new ContentHandlerpro());
+                            //读取xml的文档内容
+                            Log.d("httpreader", "parseXMLSAX: ");
+                            //第1种直接传url
+                            //xmlReader.parse(sURL);
+                            //第2种传数据
+                            OkHttpClient client = new OkHttpClient();
+                            Request request = new Request.Builder()
+                                    .url(sURL)
+                                    .build();
+                            Response response = client.newCall(request).execute();
+                            String reader = response.body().string();
+                            xmlReader.parse(new InputSource(new StringReader(reader)));
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    //SAX解析接口
+                    class ContentHandlerpro extends DefaultHandler {
+                        private String TAG = "ContentHandlerpro";
+                        private String nodeName;
+                        private StringBuilder sName;
+                        private StringBuilder iPrince;
+                        private StringBuilder result;
+
+                        @Override
+                        public void endDocument() throws SAXException {
+                            showResponse(result.toString());
+                        }
+
+                        @Override
+                        public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+                            Log.d(TAG, "startElement-localName: " + localName);
+                            Log.d(TAG, "startElement-qName: " + qName);
+                            nodeName = localName;
+                        }
+
+                        @Override
+                        public void startDocument() throws SAXException {
+                            sName = new StringBuilder();
+                            iPrince = new StringBuilder();
+                            result = new StringBuilder();
+                        }
+
+                        @Override
+                        public void endElement(String uri, String localName, String qName) throws SAXException {
+                            if ("book".equals(localName)) {
+                                result.append("书名:" + sName.toString().trim() + ";");
+                                result.append("价格:" + iPrince.toString().trim() + "\r\n");
+                                sName.setLength(0);
+                                iPrince.setLength(0);
+                            }
+                        }
+
+                        @Override
+                        public void characters(char[] ch, int start, int length) throws SAXException {
+                            if ("name".equals(nodeName)) {
+                                sName.append(ch, start, length);
+                            } else if ("price".equals(nodeName)) {
+                                iPrince.append(ch, start, length);
+                            }
+                        }
+                    }
+                    private void showResponse(final String s) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                responseTextView.setText(s);
+                            }
+                        });
+
+                    }
+                }).start();
+            }
         });
-    }
-
-    private void parseXMLSAX(String reader) {
-        //SAX解析
-        try {
-            //1.创建解析工厂
-            SAXParserFactory factory = SAXParserFactory.newInstance();
-            //2.得到解析器
-            //3得到解读器
-            XMLReader xmlReader = factory.newSAXParser().getXMLReader();
-            //设置内容处理器
-            xmlReader.setContentHandler(new ContentHandlerpro());
-            //读取xml的文档内容
-            Log.d("httpreader", "parseXMLSAX: "+reader);
-            String tmp=new String(reader);
-            xmlReader.parse(tmp);
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-    //SAX解析接口
-    class ContentHandlerpro extends DefaultHandler {
-        private String TAG="ContentHandlerpro";
-        private String nodeName;
-        private StringBuilder sName;
-        private StringBuilder iPrince;
-        private StringBuilder result;
-
-        @Override
-        public void endDocument() throws SAXException {
-            responseTextView.setText(result.toString());
-        }
-        @Override
-        public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
-            Log.d(TAG, "startElement: "+localName);
-
-            nodeName=localName;
-        }
-        @Override
-        public void startDocument() throws SAXException {
-            sName=new StringBuilder();
-            iPrince=new StringBuilder();
-            result=new StringBuilder();
-        }
-        @Override
-        public void endElement(String uri, String localName, String qName) throws SAXException {
-            if("book".equals(localName)){
-                result.append("书名:"+sName.toString().trim()+";");
-                result.append("价格:"+iPrince.toString().trim()+"\r\n");
-                sName.setLength(0);
-                iPrince.setLength(0);
-            }
-        }
-        @Override
-        public void characters(char[] ch, int start, int length) throws SAXException {
-            if("name".equals(nodeName)){
-                sName.append(ch,start,length);
-            }else if("price".equals(nodeName)){
-                iPrince.append(ch,start,length);
-            }
-        }
-
     }
 }
