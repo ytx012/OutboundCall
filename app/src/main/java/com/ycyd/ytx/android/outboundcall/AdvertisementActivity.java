973dfe1382b5f026a1aa42d371ae40a1e17c6e25
ytx012 <13607201698@139.com>
2019-01-02 09:06:48 +0800

    第五次提交

--- app/src/main/java/com/ycyd/ytx/android/outboundcall/AdvertisementActivity.java
+++ app/src/main/java/com/ycyd/ytx/android/outboundcall/AdvertisementActivity.java
@@ -1,21 +1,97 @@
 package com.ycyd.ytx.android.outboundcall;
 
-import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBar;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
-import android.view.View;
+import android.widget.TextView;
+
+import com.ycyd.ytx.android.util.LogUtils;
+import com.ycyd.ytx.android.util.NetworkUtil;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Response;
+
+import static java.lang.Thread.sleep;
 
 public class AdvertisementActivity extends AppCompatActivity {
+    private static final String TAG = "AdvertisementActivity";
+    LogUtils.sign=LogUtils.VERBOSE;
+    private TextView contentview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advertisement);
+        contentview =(TextView)findViewById(R.id.id_fullscreen_content);
+        //开始版本检查
+        try{
+            //获取packagemanager的实例
+            PackageManager packageManager = getPackageManager();
+            //getPackageName()是你当前类的包名，0代表是获取版本信息
+            String packName=getPackageName();
+            PackageInfo packInfo = packageManager.getPackageInfo(packName,PackageManager.GET_ACTIVITIES);
+            final int curVersionCode=packInfo.versionCode;
+            final String curAppName=packInfo.packageName;
+            ApplicationInfo appInfo = packageManager.getApplicationInfo(packName,PackageManager.GET_META_DATA);
+            String address = appInfo.metaData.getString("QueryVersionURL");
+            //String address = "http://111.47.64.131:8091/app/QueryVersionJSON.jsp";
+            Map<String, String> map = new HashMap<>();
+            //map.put("name", curAppName);
+            map.put("name", "Android.OutboundCall");
+            NetworkUtil.sendOkHttpRequestPost(address, map,
+                    new okhttp3.Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                            e.printStackTrace();
+                        }
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            try {
+                                String resultJson = response.body().string();
+                                final String strLastVersionCode=NetworkUtil.parseJSON(resultJson);
+                                sleep(1000);
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        contentview.setText(strLastVersionCode);
+                                    }
+                                });
+                                if(Integer.parseInt(strLastVersionCode)>curVersionCode) {
+                                    UpdateApp();
+                                }
+                                else{
+                                    StartLogin();
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                                LogUtils.d(TAG+"----------->>NetworkUtil.parseJSON","解析版本号失败！");
+                                finish();
+                            }
+                        }
+                    });
 
 
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
+    }
+
+    private void UpdateApp() {
+    }
+
+    private void StartLogin() {
+        //
+        Intent intent = new Intent(this,AdvertisementActivity.class);
+        startActivity(intent);
+        //结束掉当前的activity
+        this.finish();
     }
 
 }
--- studentnetwork/src/main/java/com/ycyd/ytx/android/studentnetwork/OkHttpActivity.java
+++ studentnetwork/src/main/java/com/ycyd/ytx/android/studentnetwork/OkHttpActivity.java
@@ -1,52 +1,17 @@
 package com.ycyd.ytx.android.studentnetwork;
 
-import android.media.session.MediaSession;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.ycyd.ytx.android.util.NetworkUtil;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.xml.sax.Attributes;
-
-import org.xml.sax.Locator;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.DefaultHandler;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserFactory;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.security.spec.ECField;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
 import okhttp3.Call;
 import okhttp3.Callback;
-import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
-
-import org.xml.sax.InputSource;
-
 import static java.lang.Thread.sleep;
 
 public class OkHttpActivity extends AppCompatActivity {
@@ -80,7 +45,7 @@ public class OkHttpActivity extends AppCompatActivity {
                         });
                     }
                 });
-                address = "http://10.28.4.176:8091/app/QueryVersionJSON.jsp";
+                address = "http://111.47.64.131:8091/app/QueryVersionJSON.jsp";
                 Map<String, String> map = new HashMap<>();
                 map.put("name", "Android.OutboundCall");
                 NetworkUtil.sendOkHttpRequestPost(address, map,
